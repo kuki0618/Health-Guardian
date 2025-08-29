b9786449000dad59321492b095525b4312b9b3d6
SHM_white <1131287310a@gmail.com>
2025-08-29 21:20:38 +0800

    添加数据库管理接口和对应的单元测试

---
+++ server/app/repositories/RepositoriesCore/IRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace RepositoriesCore
+{
+    public interface IRepository
+    {
+        string ConnectionString
+        {
+            get; set;
+        }
+        public abstract bool IsConnected();
+        public abstract bool Connect(string ConnectionString);
+        public abstract void Disconnect();
+        public abstract void Dispose();
+        public abstract IRepository Clone();
+        public abstract bool CreateRecords(string[] records);
+        public abstract bool UpdateRecord(string UUID, string record);
+        public abstract string[]? ReadRecords(string[] UUIDs);
+        public abstract bool DeleteRecords(string[] UUIDs);
+        public abstract string[]? SearchRecordsByUserId(string userId);
+        public abstract bool DatabaseIsInitialized((string type, string name)[] properties);
+        public abstract bool InitializeDatabase();
+        public static string GenerateConnectionString(string server, string database, string userId, string password)
+        {
+            var builder = new SqlConnectionStringBuilder
+            {
+                DataSource = server,
+                InitialCatalog = database,
+                UserID = userId,
+                Password = password,
+                Encrypt = true,
+                TrustServerCertificate = true,
+                ConnectTimeout = 30,
+                MultipleActiveResultSets = true
+            };
+            return builder.ConnectionString;
+        }
+    }
+}
